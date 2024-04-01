44fb9819f6f1b63dc7e4e9acb987bc57d0409669
sharmadevesh1005 <sharmadebesh1--5@gmail.com>
2024-04-01 14:30:54 +0530

    nav

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Main Navbar</div>
+    <div className="bg-[#321058] h-20 flex items-center justify-between text-[#B2B2B2] p-3">
+    <img
+      src="https://opentools.ai/build/assets/opentools-logo-3bf81de6.svg"
+      className="logo h-7"
+      alt="Logo"
+    />
+    {["Favourites", "News", "Advertise"].map((items, index) => (
+      <div className="flex justify-between items-center font-semibold text-2xl">{items}</div>
+    ))}
+    <div className="bg-[#B2B2B2] flex gap-2 px-3 py-2 rounded-full ">
+    <button className="text-[#620571] font-semibold">LogIn</button>
+    <div className="bg-[#481775] px-3 py-1 rounded-full ">Singup</div>
+    </div>
+  </div>
   )
 }
 
